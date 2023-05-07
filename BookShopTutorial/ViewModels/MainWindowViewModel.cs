458aaedb616de24031ac3a25a7fb8582ca4c52b7
Alexey Shustrov <humoressus@yandex.ru>
2023-05-07 17:14:58 +0300

    Модель главного окна и локатора

---
+++ BookShopTutorial/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathCore.ViewModels;
+
+namespace BookShopTutorial.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Главное окно программы";
+
+        private string Title { get => _Title; set => Set(ref _Title, value); }
+    }
+}
