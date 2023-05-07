15ef6207584a92ed31d38f0c2c9058ee1f7dcb61
Alexey Shustrov <humoressus@yandex.ru>
2023-05-07 17:36:01 +0300

    Контейнер сервисов

--- BookShopTutorial/App.xaml.cs
+++ BookShopTutorial/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,26 @@ namespace BookShopTutorial
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _Host;
+        public static IHost Host => _Host ??= Programm.CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+        public static IServiceProvider Services => Host.Services;
+        internal static void ConfigurateServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var host = Host;
+            base.OnStartup(e);
+            await host.StartAsync();
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            using var host = Host;
+            base.OnExit(e);
+            await host.StopAsync();
+        }
     }
 }
